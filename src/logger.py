bc7cd731d2272adf3af3682a1431ed13f9dbbde5
Shyanilgrowthens <shyanilgrowthens@gmail.com>
2025-11-26 17:52:29 +0530

    Fix exception handler and logging line number

--- src/logger.py
+++ src/logger.py
@@ -2,13 +2,22 @@ import logging
 import os
 from datetime import datetime
 
+# Reset logging (fix for Jupyter/VSCode)
+for handler in logging.root.handlers[:]:
+    logging.root.removeHandler(handler)
+
 logs_folder = os.path.join(os.getcwd(), "logs")
 os.makedirs(logs_folder, exist_ok=True)
+
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-LOG_FILE_PATH = os.path.join(logs_folder , LOG_FILE)
+LOG_FILE_PATH = os.path.join(logs_folder, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
+    level=logging.INFO,
 )
+
+if __name__ == "__main__":
+    logging.info("logging has started")
+    logging.shutdown()
