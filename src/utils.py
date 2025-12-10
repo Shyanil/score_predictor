61e121d5ef4333bca81a771ae14de65b072be5ad
Shyanil mishra <shyanilmishra@Shyanils-MacBook-Air.local>
2025-12-10 22:01:04 +0530

    Deployment configuarator

--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,7 @@ def save_obj(file_path , obj):
     try:
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path , exist_ok=True)
-        with open("file_path" , "wb") as file_obj:
+        with open(file_path , "wb") as file_obj:
             dill.dump(obj , file_obj)
     except Exception as e:
         raise custom_Exception(e, sys)
